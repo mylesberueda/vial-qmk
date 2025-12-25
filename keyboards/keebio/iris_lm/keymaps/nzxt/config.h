8867c223d5edd5ab1b232cc820b28834044a8691
Myles Berueda <myles@polypixel.io>
2025-12-24 18:26:52 -1000

    [keebio/iris_lm]: adds game layer

--- keyboards/keebio/iris_lm/keymaps/nzxt/config.h
+++ keyboards/keebio/iris_lm/keymaps/nzxt/config.h
@@ -7,11 +7,15 @@
 #define VIAL_UNLOCK_COMBO_COLS {0,5}
 
 // Memory savings
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define DYNAMIC_KEYMAP_MACRO_COUNT 4
 #define VIAL_COMBO_ENTRIES 0
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define NO_ALT_REPEAT_KEY
 
-// Split keyboard sync (so right half knows caps lock state)
+// Split keyboard sync
 #define SPLIT_LED_STATE_ENABLE
+#define SPLIT_LAYER_STATE_ENABLE
 
 // Default RGB settings (only apply after EEPROM clear)
 #define RGB_MATRIX_DEFAULT_MODE 15
